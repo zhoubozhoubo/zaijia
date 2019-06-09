f29c95b7c89d95733e2efc938c626e5bcef5e055
zhoubo759226488 <759226488@qq.com>
2019-06-10 01:48:57 +0800

    zb190609

--- src/components/User.vue
+++ src/components/User.vue
@@ -38,22 +38,22 @@
 
     <!--handle-->
     <van-row class="handle">
+      <van-col span="12" class="handle_content my_qr">
+        <div @click="goMyQr">
+          <van-icon name="gift-card-o" size="30px" color="#FF0000"/>
+          <h2>邀请有礼</h2>
+        </div>
+      </van-col>
+      <van-col span="12" class="handle_content my_qr">
+        <div @click="showSkill">
+          <van-icon name="aim" size="30px" color="#00FFFF"/>
+          <h2>邀请技巧</h2>
+        </div>
+      </van-col>
       <van-col span="12" class="handle_content my_task">
         <div @click="goMyTask">
           <van-icon name="notes-o" size="30px" color="#FFA500"/>
           <h2>我的任务</h2>
-        </div>
-      </van-col>
-      <van-col span="12" class="handle_content my_notice">
-        <div @click="goMyNotice">
-          <van-icon name="chat-o" size="30px" color="#00FF00" info="9"/>
-          <h2>最新通知</h2>
-        </div>
-      </van-col>
-      <van-col span="12" class="handle_content my_qr">
-        <div @click="goMyQr">
-          <van-icon name="qr" size="30px" color="#000000"/>
-          <h2>一键分享</h2>
         </div>
       </van-col>
       <van-col span="12" class="handle_content my_team">
@@ -62,15 +62,56 @@
           <h2>我的团队</h2>
         </div>
       </van-col>
+      <van-col span="12" class="handle_content my_notice">
+        <div @click="goMyNotice">
+          <van-icon name="chat-o" size="30px" color="#00FF00" info="9"/>
+          <h2>最新通知</h2>
+        </div>
+      </van-col>
+      <van-col span="12" class="handle_content my_notice">
+        <div @click="showService">
+          <van-icon name="phone-circle-o" size="30px" color="#808000"/>
+          <h2>联系客服</h2>
+        </div>
+      </van-col>
     </van-row>
 
     <!--otherinfo-->
     <van-row>
       <van-col span="24" class="other_info">
         <h1>慧元财富</h1>
-        <h2>客服热线：123123123</h2>
+        <h2>做任务赚现金，真的可以‘躺着赚’</h2>
       </van-col>
     </van-row>
+
+    <!--skill-->
+    <van-popup v-model="skillShow" class="skill">
+      <van-row>
+        <van-col span="24">
+          <h1>邀请技巧</h1>
+        </van-col>
+        <van-col span="24">
+          <p>1. 自己先做任务赚钱，并提现，然后分享赚钱乐趣；</p>
+          <p>2. 私下邀请家人、朋友、同学、同事加入赚钱行列，成为你的队员；</p>
+          <p>3. 分享到3个以上微信群，每天坚持发朋友圈，成功邀请几率提升200%；</p>
+          <p>4. 告诉好友：在家就能兼职赚钱，推广拿高额团队佣金；</p>
+          <p>5. 合伙人永享一级队员22%的团队佣金，永享二级队员3%的团队佣金;</p>
+        </van-col>
+      </van-row>
+    </van-popup>
+
+    <!--service-->
+    <van-popup v-model="serviceShow" class="service">
+      <van-row>
+        <van-col span="24">
+          <h1>联系客服</h1>
+        </van-col>
+        <van-col span="24">
+          <p>客服 QQ：2332155808</p>
+          <p>合作邮箱：2332155808@qq.com</p>
+        </van-col>
+      </van-row>
+    </van-popup>
   </div>
 </template>
 
@@ -79,6 +120,8 @@ export default {
   name: 'User',
   data () {
     return {
+      skillShow: false,
+      serviceShow: false
     }
   },
   methods: {
@@ -118,6 +161,14 @@ export default {
         name: 'MyQr'
       })
     },
+    showSkill () {
+      console.log('showSkill')
+      this.skillShow = true
+    },
+    showService () {
+      console.log('showService')
+      this.serviceShow = true
+    },
     goMyTeam () {
       console.log('goMyTeam')
       this.$router.push({
@@ -135,7 +186,7 @@ export default {
   .user{
     background-color: #F5F5F5;
   }
-  .user h1,h2{
+  .user h1,h2,p{
     margin: 0;
   }
   .user .user_info{
@@ -224,5 +275,39 @@ export default {
   .user .other_info h2{
     font-size: 14px;
     color: #808080;
+  }
+  .user .skill{
+    width: 300px;
+    border-radius: 5px;
+    padding: 20px;
+  }
+  .user .skill h1{
+    margin-bottom: 10px;
+    font-size: 20px;
+    color: #000;
+    text-align: center;
+    line-height: 40px;
+  }
+  .user .skill p{
+    font-size: 16px;
+    color: #696969;
+    line-height: 25px;
+  }
+  .user .service{
+    width: 300px;
+    border-radius: 5px;
+    padding: 20px;
+  }
+  .user .service h1{
+    margin-bottom: 10px;
+    font-size: 20px;
+    color: #000;
+    text-align: center;
+    line-height: 40px;
+  }
+  .user .service p{
+    font-size: 16px;
+    color: #696969;
+    line-height: 25px;
   }
 </style>
