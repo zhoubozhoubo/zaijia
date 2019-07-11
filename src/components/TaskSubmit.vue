54cf3c5473d2970bbc05bfb99948cb59c46ace18
zhoubozhoubo <759226488@qq.com>
2019-07-11 14:32:32 +0800

    zb

--- src/components/TaskSubmit.vue
+++ src/components/TaskSubmit.vue
@@ -276,8 +276,10 @@
             let localIds = res.localIds // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
             vm.formItem.submit_img = vm.formItem.submit_img.concat(res.localIds);
             alert('uploadImageStart')
+            alert(localIds)
+            alert(localIds[0])
               wx.uploadImage({
-                localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
+                localId: localIds[0], // 需要上传的图片的本地ID，由chooseImage接口获得
                 isShowProgressTips: 1, // 默认为1，显示进度提示
                 success: function (res) {
                   alert('uploadImage')
